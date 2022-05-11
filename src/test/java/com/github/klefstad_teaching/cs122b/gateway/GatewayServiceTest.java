df0b3e8a5c45706c543e7e3efdb4bea5db9eecc2
AlexanderIskander <Alex.Iskander@Gmail.com>
2022-05-11 16:05:47 -0700

    Update GatewayServiceTest.java

--- src/test/java/com/github/klefstad_teaching/cs122b/gateway/GatewayServiceTest.java
+++ src/test/java/com/github/klefstad_teaching/cs122b/gateway/GatewayServiceTest.java
@@ -35,7 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @Sql("/empty-gateway-test-data.sql")
-@AutoConfigureWebTestClient
+@AutoConfigureWebTestClient(timeout = "64000")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GatewayServiceTest
 {
